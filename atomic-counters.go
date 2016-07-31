61c983a5b7054cfa0ec4433169473099cf31f2d8
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-31 17:18:39 -0300

    Add atomic counter example

---
+++ atomic-counters.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+	"runtime"
+	"time"
+)
+
+func main() {
+	var ops uint64 = 0
+
+	for i := 0; i < 50; i++ {
+		go func() {
+			for {
+				atomic.AddUint64(&ops, 1)
+
+				runtime.Gosched()
+			}	
+		}()
+	}
+
+	time.Sleep(time.Second)
+
+	opsFinal := atomic.LoadUint64(&ops)
+	fmt.Printf("%d\n", opsFinal)
+}
