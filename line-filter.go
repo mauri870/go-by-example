5936279df6ff5db1778b29e0ec7022221894d1ee
Mauri de Souza Nunes <mauri870@gmail.com>
2016-08-16 21:08:53 -0300

    Add example line filter

---
+++ line-filter.go
@@ -0,0 +1,26 @@
+// Example usage:
+// echo whatever | go run line-filter.go
+//As you can see, it works like other filters (grep for example)
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		ucl := strings.ToUpper(scanner.Text())
+
+		fmt.Println(ucl)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
